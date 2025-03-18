e681a6ff6f27f1bdd901df792c87186da37b1f78
Patrick <ndunguricky3@gmail.com>
2025-03-18 06:16:44 -0400

    collectives

---
+++ strings.py
@@ -0,0 +1,20 @@
+# stringd
+sentence = "The quick brown fox jumped over the lazy dog"
+# builtin functionality
+# function
+print(sentence)
+print(sentence.upper()) # call a function
+print(sentence)
+print(sentence.lower())
+print(sentence.title()) #jaNe nJeri ==> Jane Njeri
+print(sentence.count("dog"))
+print(sentence.isspace())
+
+sentence = '99'
+if sentence.isspace():
+    print("Invalid name")
+else:
+    print("Valid name")
+
+
+
